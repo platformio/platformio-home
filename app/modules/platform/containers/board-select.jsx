b43761e425460cf86ffa35600722540d084d30ba
Eugene Lyulka <eugene.kiev@gmail.com>
2019-10-29 15:32:02 +0200

    Fixed platform/board-select filter callback to skip groups.
    // Resolve #26

--- app/modules/platform/containers/board-select.jsx
+++ app/modules/platform/containers/board-select.jsx
@@ -65,9 +65,16 @@ class BoardSelect extends React.Component {
         size="large"
         placeholder={`Select a board (${this.props.items.length} available)`}
         optionFilterProp="children"
-        filterOption={(input, option) =>
-          option.props.children.toLowerCase().includes(input.toLocaleLowerCase())
-        }
+        filterOption={(input, option) => {
+          const { children } = option.props;
+          if (typeof children === 'string') {
+            return option.props.children
+              .toLowerCase()
+              .includes(input.toLocaleLowerCase());
+          }
+          // Skipping groups because matched group shows all its options
+          return false;
+        }}
         onChange={::this.onDidChange}
       >
         {Object.keys(data).map(group => (
