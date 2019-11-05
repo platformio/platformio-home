914b0453e616cbb949d859ca6387503dc71dcde0
Ivan Kravets <me@ikravets.com>
2019-11-06 00:46:06 +0200

    Remove obsolate code

--- app/modules/home/sagas.js
+++ app/modules/home/sagas.js
@@ -19,22 +19,16 @@
 import * as actions from './actions';
 import * as selectors from './selectors';
 
-import { STORE_READY, updateEntity } from '../../store/actions';
-import { call, put, select, take, takeLatest } from 'redux-saga/effects';
+import { call, put, select, takeLatest } from 'redux-saga/effects';
 
 import { apiFetchData } from '../../store/api';
-import { selectStorageItem } from '../../store/selectors';
+import { updateEntity } from '../../store/actions';
 
 function* watchLoadLatestTweets() {
   yield takeLatest(actions.LOAD_LATEST_TWEETS, function*({ username }) {
     let items = yield select(selectors.selectLatestTweets);
     if (items) {
       return;
-    }
-    // if storage is not loaded yet, wait and don't block other calls
-    // 'load_latest_tweets' blocks HOME's backend
-    if (!(yield select(selectStorageItem, 'coreVersion'))) {
-      yield take(STORE_READY);
     }
     try {
       items = yield call(apiFetchData, {
