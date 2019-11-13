47a249e85893369018cc07cb7c907b5e5bf4cd94
Ivan Kravets <me@ikravets.com>
2019-11-13 15:28:16 +0200

    Temporary fix for a broken RPC "misc.load_latest_tweets" method in PIO Core 4.1.0

--- app/modules/home/sagas.js
+++ app/modules/home/sagas.js
@@ -35,9 +35,29 @@ function* watchLoadLatestTweets() {
         query: 'misc.load_latest_tweets',
         params: [`https://dl.platformio.org/tweets/${username}/data.json`]
       });
-    } catch (err) {
-      items = err;
-      console.error(err);
+    } catch (err1) {
+      console.error(err1);
+      // FIXME: Remove this code after PIO Core 4.1.1 release (begin)
+      try {
+        const cacheValid = '1d';
+        items = JSON.parse(
+          yield call(apiFetchData, {
+            query: 'os.request_content',
+            params: [
+              `https://dl.platformio.org/tweets/${username}/data.json`,
+              undefined,
+              undefined,
+              cacheValid
+            ]
+          })
+        );
+        // fallvback to backend cache
+        items = items.result ? items.result : items;
+      } catch (err2) {
+        console.error(err2);
+        items = err2;
+      }
+      // FIXME: Remove this code after PIO Core 4.1.1 release (end)
     }
     yield put(updateEntity('latestTweets', items));
   });
